5cdd4ff137c6aff1d13ead6443805d6374a5d029
panjun <1022988217@qq.com>
2018-07-03 17:17:42 +0800

    20180703

---
+++ src/components/page/Pos.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pos">
+    <h2>pos demo</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pos'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
